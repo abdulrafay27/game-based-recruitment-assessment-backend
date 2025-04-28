c07537c92919596a85762abb022f6dd88e5d04ec
Abdul Rafay <abdulrafay715@gmail.com>
2025-04-28 19:15:17 +0500

    remove feedback files

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,11 @@ const authRoutes = require("./authRoutes");
 const assessmentRoutes = require("./assessmentRoutes");
 const faqRoutes = require("./faqRoutes");
 const resultsRoutes = require("./resultsRoutes");
-const feedbackRoutes = require("./feedbackRoutes");
 
 // Use the routes
 router.use("/auth", authRoutes);
 router.use("/assessments", assessmentRoutes);
 router.use("/faqs", faqRoutes);
 router.use("/results", resultsRoutes);
-router.use("/feedback", feedbackRoutes);
 
 module.exports = router;
